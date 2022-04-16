e6debad174e115c15c2ceb5e55a6a220a137729a
rajnis0608 <rajnis0608@gmail.com>
2022-04-16 10:48:46 +0530

    Added images to list items

--- MultiScreen/app/src/main/java/com/example/multiscreen/Word.java
+++ MultiScreen/app/src/main/java/com/example/multiscreen/Word.java
@@ -1,12 +1,25 @@
 package com.example.multiscreen;
 
+import android.content.res.Resources;
+
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int imageResourceID;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
 
     public Word(String mDefaultTranslation, String mMiwokTranslation){
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
+        imageResourceID = NO_IMAGE_PROVIDED;
+    }
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation,int image){
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.imageResourceID = image;
     }
 
     public String getDefaultTranslation(){
@@ -15,5 +28,11 @@ public class Word {
 
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceID() { return imageResourceID; }
+
+    public boolean hasImage() {
+        return imageResourceID != -1;
     }
 }
--- MultiScreen/app/src/main/java/com/example/multiscreen/WordAdapter.java
+++ MultiScreen/app/src/main/java/com/example/multiscreen/WordAdapter.java
@@ -1,15 +1,54 @@
 package com.example.multiscreen;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.app.Activity;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
+
+    private int colorResourceID;
+    public WordAdapter(Activity context, ArrayList<Word> words,int color){
+        super(context,0,words);
+        this.colorResourceID = color;
+    }
     @Override
     public View getView(int position,View convertView,ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if (currentWord.hasImage()) {
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setImageResource(currentWord.getImageResourceID());
+        }else {
+            imageView.setVisibility(View.GONE);
+        }
+
+        LinearLayout textContainer = (LinearLayout) listItemView.findViewById(R.id.text_container);
+
+        int color = ContextCompat.getColor(getContext(),colorResourceID);
+
+        textContainer.setBackgroundColor(color );
+        return listItemView;
     }
 
 }
