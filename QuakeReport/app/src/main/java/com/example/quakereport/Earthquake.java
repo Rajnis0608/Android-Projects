e847cbf88a9ef307d34f99bac1d5ba59e83b15da
rajnis0608 <rajnis0608@gmail.com>
2022-04-24 23:10:36 +0530

    Layout ploishing and intent to move to webpage

---
+++ QuakeReport/app/src/main/java/com/example/quakereport/Earthquake.java
@@ -0,0 +1,28 @@
+package com.example.quakereport;
+
+public class Earthquake {
+
+    private double magnitude;
+
+    private String location;
+
+    private long timeInMilliseconds;
+
+    private String url;
+
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.timeInMilliseconds = timeInMilliseconds;
+        this.url = url;
+    }
+
+    public double getMagnitude() { return magnitude; }
+
+    public String getLocation() { return location; }
+
+    public long getTimeInMilliseconds() {return timeInMilliseconds; }
+
+    public String getUrl() { return url; }
+
+}
--- QuakeReport/app/src/main/java/com/example/quakereport/EarthquakeActivity.java
+++ QuakeReport/app/src/main/java/com/example/quakereport/EarthquakeActivity.java
@@ -1,7 +1,11 @@
 package com.example.quakereport;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,24 +21,41 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_earthquake);
 
         // Create a fake list of earthquake locations.
-        ArrayList<String> earthquakes = new ArrayList<>();
-        earthquakes.add("San Francisco");
-        earthquakes.add("London");
-        earthquakes.add("Tokyo");
-        earthquakes.add("Mexico City");
-        earthquakes.add("Moscow");
-        earthquakes.add("Rio de Janeiro");
-        earthquakes.add("Paris");
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+//        earthquakes.add(new Earthquake("7.2","San Francisco","Feb 2,2016"));
+//        earthquakes.add(new Earthquake("6.1","London","July 20,2015"));
+//        earthquakes.add(new Earthquake("3.5","Tokyo","Nov 10, 2014"));
+//        earthquakes.add(new Earthquake("3.4","Mexico City","May 3, 2014"));
+//        earthquakes.add(new Earthquake("2.8","Moscow","Jan 31, 2013"));
+//        earthquakes.add(new Earthquake("4.9","Rio de Janeiro","Aug 19, 2012"));
+//        earthquakes.add(new Earthquake("1.6","Paris","Oct 30, 2011"));
+
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
-        // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, earthquakes);
-
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
         earthquakeListView.setAdapter(adapter);
+
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current earthquake that was clicked on
+                Earthquake currentEarthquake = adapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+
+
+
     }
 }
